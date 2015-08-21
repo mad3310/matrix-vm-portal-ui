4d2dd7658fb9390166c6a192edafb2a63f168e97
yaokuo <yaokuo@letv.com>
2015-08-21 15:22:44 +0800

    调整路由

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var request = require("request");
 
 var routes = require('./routes/index');
 var api = require('./routes/api');
+var proxy = require ('./routes/proxy');
 
 var app = express();
 
@@ -19,9 +19,9 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(function (req, res, next) {
-  var authResult = true;
-  if (authResult == false) return next(new Error('权限不足'));
-  next();
+    var authResult = true;
+    if (authResult == false) return next(new Error('权限不足'));
+    next();
 });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -31,11 +31,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/api', api);
 
+// proxy to backend server request
+app.use(proxy);
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -46,31 +49,21 @@ console.log(app.get('env'));
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        if(err.status === 404){
-            var p = 'http://'+'127.0.0.1:8080'+req.url;
-//            req.headers['Host'] = req.headers.host;
-            request({
-                method:req.method,
-                url:p,
-                headers:req.headers
-            }).pipe(res);
-        }else{
-            res.render('error', {
-                message: err.message,
-                error: err
-            });
-        }
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
