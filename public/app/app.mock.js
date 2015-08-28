214154ee6844d12e734a478447c6bc479c587105
jiangfei5945 <jiangfei5945@hotmail.com>
2015-08-28 17:11:23 +0800

    ngrap调研

--- public/app/app.mock.js
+++ public/app/app.mock.js
@@ -1,7 +1,7 @@
 define(['app'], function (app) {
   app.config(['$httpProvider', 'ngRapProvider', function (httpProvider, ngRapProvider) {
     // replace nnn with your project id
-    ngRapProvider.script = 'http://rap.alibaba-inc.com/rap.plugin.js?projectId=nnn';
+    ngRapProvider.script = 'http://10.154.28.164:17070/rap.plugin.js?projectId=4';
     ngRapProvider.enable({
       mode: 3
     });
